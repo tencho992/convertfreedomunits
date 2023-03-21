61f82a26cf2954e061383d33d2da08519e567589
Tenzin Chosang <tenzinchosang.dev@gmail.com>
2023-03-21 12:16:16 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,116 @@
+
+// document.querySelector('#convert').addEventListener('click', conversion)
+
+// function conversion(){
+//     let celsius= document.querySelector('#celsius').value
+//     let total= (celsius * 1.8) +32 //must multiply first
+//     document.querySelector('#conversionDisplay').innerText= total
+// }
+
+
+// document.querySelector('#convert').addEventListener('click', conversion)
+
+// function conversion(){
+//     let celsius = document.querySelector('#celsius').value
+//     let total = (celsius *1.8) +32 
+//     document.querySelector('#conversionDisplay').innerText= total
+// }
+
+
+// document.querySelector('#convert').addEventListener('click', conversion)
+// function conversion(){
+//     let celsius= document.querySelector('#celsius').value
+//     let total = (celsius * 1.8 ) + 32
+//     document.querySelector('#conversionDisplay').innerText= total
+//}
+
+// document.querySelector('#convert').addEventListener('click', conversion)
+// function conversion(){
+//     let celsius= document.querySelector('#celsius').value
+//     let total = (celsius * 1.8 ) + 32
+//     document.querySelector('#conversionDisplay').innerText= total
+//}
+
+// document.querySelector('#convert').addEventListener('click', conversion)
+// function conversion(){
+//     let celsius= document.querySelector('#celsius').value
+//     let total = (celsius * 1.8 ) + 32
+//     document.querySelector('#conversionDisplay').innerText= total
+//}
+
+// document.querySelector('#convert').addEventListener('click', conversion)
+// function conversion(){
+//     let celsius= document.querySelector('#celsius').value
+//     let total = (celsius * 1.8 ) + 32
+//     document.querySelector('#conversionDisplay').innerText= total
+//}
+
+// document.querySelector('#convert').addEventListener('click', conversion)
+// function conversion(){
+//     let celsius= document.querySelector('#celsius').value
+//     let total = (celsius * 1.8 ) + 32
+//     document.querySelector('#conversionDisplay').innerText= total
+//}
+
+// document.querySelector('#convert').addEventListener('click', conversion)
+// function conversion(){
+//     let celsius= document.querySelector('#celsius').value
+//     let total = (celsius * 1.8 ) + 32
+//     document.querySelector('#conversionDisplay').innerText= total
+//}
+
+// document.querySelector('#convert').addEventListener('click', conversion)
+// function conversion(){
+//     let celsius= document.querySelector('#celsius').value
+//     let total = (celsius * 1.8 ) + 32
+//     document.querySelector('#conversionDisplay').innerText= total
+//}
+
+// document.querySelector('#convert').addEventListener('click', conversion)
+// function conversion(){
+//     let celsius= document.querySelector('#celsius').value
+//     let total = (celsius * 1.8 ) + 32
+//     document.querySelector('#conversionDisplay').innerText= total
+//}
+
+// document.querySelector('#convert').addEventListener('click', conversion)
+// function conversion(){
+//     let celsius= document.querySelector('#celsius').value
+//     let total = (celsius * 1.8 ) + 32
+//     document.querySelector('#conversionDisplay').innerText= total
+//}
+
+// document.querySelector('#convert').addEventListener('click', conversion)
+// function conversion(){
+//     let celsius= document.querySelector('#celsius').value
+//     let total = (celsius * 1.8 ) + 32
+//     document.querySelector('#conversionDisplay').innerText= total
+//}
+
+// document.querySelector('#convert').addEventListener('click', conversion)
+// function conversion(){
+//     let celsius= document.querySelector('#celsius').value
+//     let total = (celsius * 1.8 ) + 32
+//     document.querySelector('#conversionDisplay').innerText= total
+//}
+
+// document.querySelector('#convert').addEventListener('click', conversion)
+// function conversion(){
+//     let celsius= document.querySelector('#celsius').value
+//     let total = (celsius * 1.8 ) + 32
+//     document.querySelector('#conversionDisplay').innerText= total
+//}
+
+// document.querySelector('#convert').addEventListener('click', conversion)
+// function conversion(){
+//     let celsius= document.querySelector('#celsius').value
+//     let total = (celsius * 1.8 ) + 32
+//     document.querySelector('#conversionDisplay').innerText= total
+//}
+
+document.querySelector('#convert').addEventListener('click', conversion)
+function conversion(){
+    let celsius = Number(document.querySelector('#celsius').value)
+    let total = (celsius * 1.8 ) + 32
+    document.querySelector('#conversionDisplay').innerText= total
+}
